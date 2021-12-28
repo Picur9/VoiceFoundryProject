82f717cd3f3a5caf2780314f17828c3f711529aa
Picur9 <78418713+Picur9@users.noreply.github.com>
2021-12-28 20:27:29 +0000

    Update test.py

--- test.py
+++ test.py
@@ -15,11 +15,11 @@ class TestVanityPhone(unittest.TestCase):
         self.assertEqual(vanity.generate("+13569377"), ['1flowers', '13lowers', '1el6yeps', '1el6zeps', '1elm9err'])
 
      def test_vanity_advance(self):
-        self.assertEqual(vanity.generate("+46244"), ['4magi', 'go2hi', 'hm2hi', 'ho2hi', 'in2hi'])
-        self.assertEqual(vanity.generate("46244"), ['4magi', 'go2hi', 'hm2hi', 'ho2hi', 'in2hi'])
-        self.assertEqual(vanity.generate("+6086657"), ['60tools', '608mols', '60took7', '60tool7', '608mol7'])
-        self.assertEqual(vanity.generate("+13569377"), ['1flowers', '13lowers', '1el6yeps', '1el6zeps', '1elm9err'])
-        
-        
+        self.assertEqual(vanity_advance.generate("+46244"), ['4-magi', 'inch-4', 'I-magi', 'inch-I', 'go-ahi'])
+        self.assertEqual(vanity_advance.generate("46244"), ['4-magi', 'inch-4', 'I-magi', 'inch-I', 'go-ahi'])
+        self.assertEqual(vanity_advance.generate("+6086657"), ['+60-tools', '+608-mols', '+60-took-R', '+60-tool-R', '+60-U-mols'])
+        self.assertEqual(vanity_advacne.generate("+13569377"), ['+1-flowers', '+1-flower-R', '+1-3-lowers', '+1-flower-7', '+1-el-oyers'])
+        self.assertEqual(vanity_advance.generate("+120605"), [])
+                
 if __name__ == '__main__':
     unittest.main()
