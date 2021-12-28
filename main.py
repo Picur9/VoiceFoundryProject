444f7e9b904d213c7f35a0da58845267d72cdfc3
Picur9 <78418713+Picur9@users.noreply.github.com>
2021-12-28 20:30:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ def main(argv):
     print(f"Vanity numbers for {argv}:", *resp)
     print(f"Generation took {end - start:0.4f} seconds")
  
-def main(argv):
     start = time.perf_counter()
     resp = vanity_advance.generate(argv)
     end = time.perf_counter()
-    print(f"Vanity numbers for {argv}:", *resp)
+    print(f"Vanity advance numbers for {argv}:", *resp)
     print(f"Generation took {end - start:0.4f} seconds")
 
 if __name__ == "__main__":
