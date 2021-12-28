2c7549b2a8c09acdf1dc4ed0e247f84defc471ab
Picur9 <78418713+Picur9@users.noreply.github.com>
2021-12-28 20:29:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
 import time
 import vanity
+import vanity_advance
 
 def main(argv):
     start = time.perf_counter()
     resp = vanity.generate(argv)
+    end = time.perf_counter()
+    print(f"Vanity numbers for {argv}:", *resp)
+    print(f"Generation took {end - start:0.4f} seconds")
+ 
+def main(argv):
+    start = time.perf_counter()
+    resp = vanity_advance.generate(argv)
     end = time.perf_counter()
     print(f"Vanity numbers for {argv}:", *resp)
     print(f"Generation took {end - start:0.4f} seconds")
